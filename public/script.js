a5e21edad31a9ac52486f7fe15eaf006245b1bf0
dlanorlecram <ronald.marcel.mr@gmail.com>
2017-10-27 10:41:39 +0200

    débug sur sidebar left

--- public/script.js
+++ public/script.js
@@ -3,14 +3,12 @@ $(document).ready(function(){
   $(document)
     .on('click', '[data-show]', function(e){
       e.preventDefault()
-      console.log('je suis show')
       var tag = $('#'+$(this).data('show'));
       tag.addClass('opened');
       e.stopPropagation()
     })
     .on('click', '[data-hide]', function(e){
       e.preventDefault()
-      console.log('je suis hide')
       var tag = $('#'+$(this).data('hide'))
       tag.removeClass('opened')
       e.stopPropagation()
